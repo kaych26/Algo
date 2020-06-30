830a03a41ecd4199a40ce2700f463c88f38d5996
kay <kaych26@gmail.com>
2020-06-29 22:49:31 -0400

    updated codeSignal_reverseInParentheses to pass all test cases

--- codeSignal_reverseInParentheses.js
+++ codeSignal_reverseInParentheses.js
@@ -1,66 +1,30 @@
 function reverseInParentheses(inputString) {
-  let i = 0;
   let s = inputString;
-  let result = '';
-
-  const reverseParen = (w) => {
-    let word = [];
-    let j=0
-
-    while (j < w.length ) {
-      const letter = w[j];
-      if (letter === ')') {
-        // const rr = word.reverse().join('');
-        const rr = word.reverse();
-        j += 1;
-        return rr;
-        
-      }
-      else if (letter === '(') {
-        // j += 1;
-        i += 1;
-        const reverse = reverseParen(w.substring(j+1, w.length));
-        
-        reverse.forEach(l => { word.push(l); })
-        j += reverse.length+1;
-      }
-      else {
-        word.push(letter);
-      }
-      j += 1;
-    }
-    return word;
-  }
+  let i = 0;
+  let word = [];
 
   while (i < s.length) {
-    if (s[i] !== '(' && s[i] !== ')') {
-      result += s[i];
+    if (s[i] === ')') {
+
+      word.push(s[i]);
+      const reverse = word.reverse();
+      i += 1;
+      return reverse;
+    }
+    else if (s[i] === '(') {
+      word.push(s[i]);
+      let reverse = reverseInParentheses(s.substring(i + 1, s.length));
+      reverse.forEach(l => {  word.push(l); })
+      i += reverse.length;
     }
     else {
-      const reverse = reverseParen(s.substring(i + 1, s.length));
- 
-      result += reverse.join('');
-      i += reverse.length+1;
-      // i += 1;
+      word.push(s[i]);
     }
     i += 1;
   }
-  return result;
+  
+  return word.filter(char=> { return char != '(' && char != ')'}).join('');
 }
-
-// function reverseInParentheses(inputString) {
-//   let str = inputString;
-
-//   (str.match(/\((.+)\)/g) || []).forEach(a => {
-//     a = a.substring(1, a.length - 1);
-//     let b = reverseInParentheses(a)
-//         // .replace(/[\(\)]/g, function ($1) { return $1 === '(' ? ')' : '(' })
-//         .split("").reverse().join("");
-//     str = str.replace(a, b);
-// });
-// return str;
-
-// }
 
 // console.log(reverseInParentheses("one (ruof ((rht)ee) owt)"));
 // console.log(reverseInParentheses("one(abc(xyz))"));
